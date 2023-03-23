7d676ed8baf1556ef499d40b4348a12838d706f8
Dejan Aksovic <imdejan00@gmail.com>
2023-03-23 02:37:20 +0100

    Finished sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  imageUrl?:string="";
 
+  constructor() {
+    this.imageUrl = "https://www.dunp.np.ac.rs/wp-content/uploads/2022/09/logo_plavi-cirilica-206x56.png"
+  }
+
+  changeLogo(input:HTMLInputElement) {
+    input.value ? this.imageUrl = input.value : "";
+  }
 }
